320b32ade06a260ff06131f138c7091a6d117237
Zack Hanni <zack.hannI@gmail.com>
2025-04-28 19:01:23 -0400

    no longer need debug mode

--- auth.ts
+++ auth.ts
@@ -2,6 +2,5 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  debug: true,
   providers: [Google],
 });
