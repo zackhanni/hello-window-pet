dccd51284e145ed506d46e4a2d6e92ed6fe0a1cd
Zack Hanni <zack.hannI@gmail.com>
2025-04-28 14:50:53 -0400

    adjusted card sizes and centering

--- app/cats/page.tsx
+++ app/cats/page.tsx
@@ -22,7 +22,7 @@ const page = async () => {
       <h1 className="text-center text-3xl font-bold pb-4">
         Check out some recent cats!
       </h1>
-      <section className="space-y-4">
+      <section className="flex items-center justify-center flex-col md:flex-row gap-8">
         {allCats.map((cat) => (
           <CatCard cat={cat} key={cat.id} />
         ))}
@@ -44,7 +44,7 @@ const CatCard = async ({ cat }: { cat: Cat }) => {
   const photos = await getCatPhotos(cat.id);
 
   return (
-    <Card className="flex flex-col items-center justify-center">
+    <Card className="flex flex-col items-center justify-center max-w-md">
       <CardHeader className="w-full">
         <CardTitle>Name: {cat.name}</CardTitle>
         <CardDescription>
@@ -53,11 +53,17 @@ const CatCard = async ({ cat }: { cat: Cat }) => {
       </CardHeader>
       <CardContent className="grid gap-4">
         <ImageKitProvider urlEndpoint="https://ik.imagekit.io/assortfit">
-          <Image src={cat.photoUrl} width={300} height={300} alt={cat.name} />
+          <Image
+            src={cat.photoUrl}
+            width={300}
+            height={300}
+            alt={cat.name}
+            className="max-h-[300px] object-cover"
+          />
         </ImageKitProvider>
       </CardContent>
-      <CardFooter>
-        <Link href={`/cats/${cat.id}`}>
+      <CardFooter className="w-full">
+        <Link href={`/cats/${cat.id}`} className="w-full">
           <Button className="w-full">Visit this cat!</Button>
         </Link>
       </CardFooter>
