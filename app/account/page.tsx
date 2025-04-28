c571dfa76f75e5f9b5d1c654b196dac0e4e76275
Zack Hanni <zack.hannI@gmail.com>
2025-04-28 19:50:27 -0400

    made account only accessible if user is signed in

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,30 +1,43 @@
+import { auth } from "@/auth";
 import { AnimalCard } from "@/components/AnimalCard";
 import { GeneratePDF } from "@/components/GeneratePDF";
+import { SignOut } from "@/components/SignOut";
 import { Button } from "@/components/ui/button";
 import { getAllCats } from "@/lib/cats";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const Account = async () => {
   const allCats = await getAllCats();
-
+  const session = await auth();
   //   function handleDelete(id: string) {
   //     // delete listing
   //     console.log("Delete cat id: ", id);
   //   }
 
+  if (!session?.user) redirect("/");
+
   return (
     <div className="py-8 space-y-8">
-      <section>
-        <div>
-          <p>User Name</p>
-          <p>User Location</p>
-          <p>Account opened date</p>
-          <p>Delete my account</p>
+      <section className="flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+          {session.user.image && (
+            <Image
+              src={session.user.image}
+              height={50}
+              width={50}
+              alt={"profile image"}
+            />
+          )}
+          <p>{session.user.name}</p>
+          <p className="pb-2">{session.user.email}</p>
+          <SignOut />
         </div>
       </section>
       <hr />
       <section>
-        <p className="text-3xl font-bold pb-4">My Animals</p>
+        <p className="text-3xl font-bold pb-4 text-center">My Animals</p>
         <div className="flex items-center justify-center flex-col md:flex-row gap-8">
           {allCats.map((cat) => (
             <div key={cat.id} className="flex flex-col">
