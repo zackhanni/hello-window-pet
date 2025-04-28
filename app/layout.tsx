9cd97ddc614ddf133bd7bc812c3d0cb0da8ad4c7
Zack Hanni <zack.hannI@gmail.com>
2025-04-28 19:49:42 -0400

    created navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
+import Navigation from "@/components/Navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ const RootLayout = async ({
   return (
     <html lang="en">
       <SessionProvider session={session}>
+        <Navigation session={session} />
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
---
+++ components/Navigation.tsx
@@ -0,0 +1,28 @@
+import { Session } from "next-auth";
+import Link from "next/link";
+
+const Navigation = ({ session }: { session: Session }) => {
+  return (
+    <nav className="sticky top-0 bg-black text-white">
+      <ul className="flex items-center justify-center">
+        <li className={`p-4 hover:underline`}>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li className={`p-4 hover:underline`}>
+          <Link href={"/cats"}>All cats</Link>
+        </li>
+        {session?.user ? (
+          <li className={`p-4 hover:underline`}>
+            <Link href={"/account"}>My account</Link>
+          </li>
+        ) : (
+          <li className={`p-4 hover:underline`}>
+            <Link href={"/api/auth/signin"}>Make Account</Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
