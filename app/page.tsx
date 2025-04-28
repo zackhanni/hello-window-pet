8bb5ced09ef9a92ed635347ffc8204f4ccc2d833
Zack Hanni <zack.hannI@gmail.com>
2025-04-28 19:50:42 -0400

    change home text depending on if user is logged in

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,16 @@ const Home = async () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-3xl">Did you see my window cat?</h1>
-
-        <p className="bg-red-200 text-black">Hello {session?.user?.name}</p>
+        <h1 className="text-3xl">
+          {session?.user
+            ? `Welcome back, ${session?.user?.name}`
+            : "Did you see a window cat?"}
+        </h1>
 
         <p className="dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-          Check out window cats below! Or, sign up for your own cat account to
-          let people see and share images of your cat.
+          {session?.user
+            ? "You can add, update, or remove any of your animal listings in your account settings."
+            : "Check out window cats below! Or, sign up for your own cat account to let people see and share images of your cat."}
         </p>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row w-full">
@@ -30,25 +33,18 @@ const Home = async () => {
             </Button>
           </Link>
 
-          <SignIn />
-
-          {/* <Link href={"/account"} className="w-full">
-            <Button
-              variant={"secondary"}
-              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto md:w-[158px] w-full"
-            >
-              Sign Up
-            </Button>
-          </Link> */}
-
-          {/* <Link href={"/account"} className="w-full">
-            <Button
-              variant={"secondary"}
-              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto md:w-[158px] w-full"
-            >
-              Sign In
-            </Button>
-          </Link> */}
+          {session ? (
+            <Link href={"/account"} className="w-full">
+              <Button
+                variant={"secondary"}
+                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto md:w-[158px] w-full"
+              >
+                My account
+              </Button>
+            </Link>
+          ) : (
+            <SignIn />
+          )}
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
