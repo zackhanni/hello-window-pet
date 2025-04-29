30e2fa1a4204a70f61750e663123b860a45c7749
Zack Hanni <zack.hannI@gmail.com>
2025-04-29 16:07:02 -0400

    account now retrieves a user's actual pets

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -4,13 +4,12 @@ import { GeneratePDF } from "@/components/GeneratePDF";
 import HowItWorks from "@/components/HowItWorks";
 import { SignOut } from "@/components/SignOut";
 import { Button } from "@/components/ui/button";
-import { getAllCats } from "@/lib/cats";
+import { getUserPetsByEmail } from "@/lib/cats";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const Account = async () => {
-  const allCats = await getAllCats();
   const session = await auth();
   //   function handleDelete(id: string) {
   //     // delete listing
@@ -18,6 +17,8 @@ const Account = async () => {
   //   }
 
   if (!session?.user) redirect("/");
+
+  const pets = await getUserPetsByEmail(session.user.email);
 
   return (
     <main className="py-8 space-y-8">
@@ -42,11 +43,11 @@ const Account = async () => {
       <section>
         <p className="text-3xl font-bold pb-4 text-center">My Animals</p>
         <div className="flex items-center justify-center flex-col md:flex-row gap-8">
-          {allCats.map((cat) => (
-            <div key={cat.id} className="flex flex-col">
-              <AnimalCard animal={cat} />
+          {pets.map((animal) => (
+            <div key={animal.id} className="flex flex-col">
+              <AnimalCard animal={animal} />
 
-              <GeneratePDF animal={cat} />
+              <GeneratePDF animal={animal} />
 
               <Button
                 variant={"destructive"}
